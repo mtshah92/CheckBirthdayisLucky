50f5c9c75c0afacf5a1ccde363d143c8ba961c66
mtshah92 <109501983+mtshah92@users.noreply.github.com>
2022-10-24 15:07:05 +0530

    refactor

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 const birthDay = document.querySelector("#birthday");
 const luckyNumber = document.querySelector("#lucky-no");
 const checkBtn = document.querySelector("#check-btn");
-const outputtxt=document.querySelector("#text");
+const outputtxt = document.querySelector("#text");
 
 function checkBirthdayIsLucky() {
-  const dob= birthday.value;
-  const sum=calculateSum(dob);
-  if(sum&&dob)
-compareValues(sum,luckyNumber.value)
-  else
-  outputtxt.innerText="Please Enter both fields";
-  
-  
+  const dob = birthDay.value;
+  const sum = calculateSum(dob);
+  const valueOfLuckyNumber = luckyNumber.value;
+
+  if (dob !== "" && valueOfLuckyNumber !== "") {
+    if (valueOfLuckyNumber > 0) {
+      compareValues(sum, valueOfLuckyNumber);
+    } else {
+      outputtxt.innerText = "Lucky Number Should be Greater than zero";
+    }
+  } else {
+    outputtxt.innerText = "Please Enter both fields";
+  }
 }
 
-function compareValues(sum,luckynumber){
-  if(sum%luckynumber===0){
-    outputtxt.innerText="Your Birthday is Lucky";
-  }
-  else{
-    outputtxt.innerText="Not Lucky";
+function compareValues(sum, luckynumber) {
+  if (sum % luckynumber === 0) {
+    outputtxt.innerText = "Your Birthday is Lucky";
+  } else {
+    outputtxt.innerText = "OOPS ! Its not Lucky";
   }
 }
 
 function calculateSum(dob) {
-  dob=dob.replaceAll("-",0);
-  let sum=0;
-  for(let i=0;i<dob.length;i++){
-    sum=sum+Number(dob.charAt(i))
+  dob = dob.replaceAll("-", 0);
+  let sum = 0;
+  for (let i = 0; i < dob.length; i++) {
+    sum = sum + Number(dob.charAt(i));
   }
-  return sum; 
-  
+  return sum;
 }
 
-checkBtn.addEventListener('click', checkBirthdayIsLucky);
+checkBtn.addEventListener("click", checkBirthdayIsLucky);
